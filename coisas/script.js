dc3b17c830565e32a418e397cf2c0bd9f418d333
Vinícius Rosa de Lima <113705995+vinnyhbr@users.noreply.github.com>
2023-11-05 17:31:16 -0300

    add js

---
+++ coisas/script.js
@@ -0,0 +1,20 @@
+function somar(){
+    let ppQuant = document.getElementById('PP').value;
+    let pQuant = document.getElementById('P').value;
+    let mQuant = document.getElementById('M').value;
+    let gQuant = document.getElementById('G').value;
+
+    let tableSize = table.rows.length;
+    let row = table.insertRow(tableSize); 
+    let cell1 = row.insertCell(0); 
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+    let cell6 = row.insertCell(5);
+    row.id = tableSize;
+    
+   }
+   function subtrair(){
+        
+    }
